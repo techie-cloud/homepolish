9afe43d16fa49b64a2af4994b7fa96383c296576
techie-cloud <reveloper.dev@gmail.com>
2019-11-19 08:16:23 +0500

    Updated env

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.post("/form", (req, res) => {
 
 });
 
-app.listen(3000, function() {
-    console.log('Listening on port 3000', );
+app.listen(process.env.PORT, function() {
+    console.log('Listening on port '+process.env.PORT );
 });
